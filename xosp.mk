3d2f19f5e9bcc978f5632b1ab934499316cb74d2
nguyễn hoàng vĩnh thịnh <nthinh1102@gmail.com>
2016-12-29 14:24:15 +0700

    Xosp n bringup

--- xosp.mk
+++ xosp.mk
@@ -1,9 +1,10 @@
 $(call inherit-product, device/asus/Z00L/full_Z00L.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk)
 
-PRODUCT_NAME := cm_Z00L
+PRODUCT_NAME := xosp_Z00L
 PRODUCT_BRAND := asus
 PRODUCT_MANUFACTURER := asus
 PRODUCT_DEVICE := Z00L
