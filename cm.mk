6394932aeeec70412c01ec73aa6569dcc2c04e8b
nguyễn hoàng vĩnh thịnh <nthinh1102@gmail.com>
2017-01-01 20:28:26 +0700

    dm nham

--- cm.mk
+++ cm.mk
@@ -1,10 +1,9 @@
 $(call inherit-product, device/asus/Z00L/full_Z00L.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
-$(call inherit-product, vendor/xosp/config/xosp.mk)
+$(call inherit-product, vendor/cm/configs/common_full_phone.mk)
 
-PRODUCT_NAME := xosp_Z00L
+PRODUCT_NAME := cm_Z00L
 PRODUCT_BRAND := asus
 PRODUCT_MANUFACTURER := asus
 PRODUCT_DEVICE := Z00L
